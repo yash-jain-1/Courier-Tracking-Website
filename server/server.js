91d89a2feb5e8c540420090de79cb61bfc4b7c6b
Yash Jain <yashjainstudy@gmail.com>
2024-12-12 01:41:38 +0530

    Removed all secrets

--- server/server.js
+++ server/server.js
@@ -96,5 +96,5 @@ app.post('/api/shipments/:trackingNumber/updates', async (req, res) => {
 });
 
 // Define port
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
